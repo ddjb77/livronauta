0543fec839ddb3a8226437859eff903b49a674e2
ddjb77 <67380362+ddjb77@users.noreply.github.com>
2023-06-14 09:29:17 -0300

    Update niveis.js

--- src/main/resources/static/js/niveis.js
+++ src/main/resources/static/js/niveis.js
@@ -1,4 +1,12 @@
 function abrirNivelCard(){
-	var modalNivel = document.querySelector(".modal-container");
-	 modalNivel.style.display = "block"
+	var modalNivel = document.querySelector("#modal-nivel");
+	 modalNivel.style.display = "block";
+	 
+	    var okNivel = document.getElementById("okNivel");
+    okNivel.addEventListener("click", fecharModalnv);
+}
+
+function fecharModalnv() {
+    var modalNivel = document.querySelector("#modal-nivel");
+    modalNivel.style.display = "none";
 }
